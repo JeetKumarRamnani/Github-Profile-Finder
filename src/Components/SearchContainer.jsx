03b61e7094e50c0b33c2631c0bae61ece604f90f
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-09-11 20:20:33 +0500

    fixed network bugs

--- src/Components/SearchContainer.jsx
+++ src/Components/SearchContainer.jsx
@@ -3,18 +3,56 @@ import React, { useState } from "react";
 function SearchContainer({ setUIData }) {
   const [searchData, setSearchData] = useState("");
   const userQuery = searchData.split(" ").join("");
-  const token = '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';
+  const token = '';
+
+  const exampleResponse= {
+    "login": "JeetKumarRamnani",
+    "id": 110333842,
+    "node_id": "U_kgDOBpOPkg",
+    "avatar_url": "https://avatars.githubusercontent.com/u/110333842?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/JeetKumarRamnani",
+    "html_url": "https://github.com/JeetKumarRamnani",
+    "followers_url": "https://api.github.com/users/JeetKumarRamnani/followers",
+    "following_url": "https://api.github.com/users/JeetKumarRamnani/following{/other_user}",
+    "gists_url": "https://api.github.com/users/JeetKumarRamnani/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/JeetKumarRamnani/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/JeetKumarRamnani/subscriptions",
+    "organizations_url": "https://api.github.com/users/JeetKumarRamnani/orgs",
+    "repos_url": "https://api.github.com/users/JeetKumarRamnani/repos",
+    "events_url": "https://api.github.com/users/JeetKumarRamnani/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/JeetKumarRamnani/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Jeet Kumar Ramnani",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": "Web Developer And Musician",
+    "twitter_username": null,
+    "public_repos": 49,
+    "public_gists": 0,
+    "followers": 6,
+    "following": 1,
+    "created_at": "2022-07-31T10:46:19Z",
+    "updated_at": "2023-09-09T21:38:53Z"
+}
 
   async function handleSearchSubmit() {
     try {
       const API_URL = `https://api.github.com/users/${userQuery}`;
-      const response = await fetch(API_URL, {
-        headers: {
-          Authorization: `Bearer ${token}`, 
-        },
-      });
+      const response = await fetch(API_URL
+    //     , {
+    //     headers: {
+    //       Authorization: `Bearer ${token}`, 
+    //     },
+    //   }
+    );
 
       if (!response.ok) {
+        setUIData(exampleResponse)
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
